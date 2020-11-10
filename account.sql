7f2e0dc6648c6c5442acbacda23884f879a461c0
cx87974 <60827108+cx87974@users.noreply.github.com>
2020-11-10 14:25:09 -0500

    Add files via upload

--- account.sql
+++ account.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE IF NOT EXISTS acc_db;
 
-CREATE TABLE `users` (
+
+
+CREATE TABLE acc_db.`users` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(25) NOT NULL
-  'confirmed?' int(1) NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
