8352e9bfed746905d346bcbde771dc2786472eaa
Robert Roibu <roibu1@umbc.edu>
2020-11-10 15:56:35 -0500

    update

--- emailS.js
+++ emailS.js
@@ -2,31 +2,18 @@ document.getElementById("email").onclick = function() {sendEmail()};
 
 
 function sendEmail(){
-
-    var nodemailer = require('nodemailer');
-
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-        user: 'roibu1@umbc.edu',
-        pass: 'Clone1232'
-    }
-    });
-
-    var mailOptions = {
-        from: 'roibu1@gmail.com',
-        to: 'roibu206@gmail.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-    }); 
+    console.log("sending email")
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: 'roibu1@umbc.edu',
+        Password: "ElaR1776", // need to change
+        To: 'roibu1@umbc.edu',
+        From: 'roibu1@umbc.edu' ,
+        Subject : "test",
+        Body : "this is a test",
+    }).then(
+        message => alert("mail sent")
+    );
 
 
 }
